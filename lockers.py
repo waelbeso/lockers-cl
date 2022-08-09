3e3d8e79daec28c7cb45a0eab9a748f77642adc3
Wael El-Begearmi <waelabbas@live.com>
2022-08-09 06:33:54 +0200

    update
    update

--- lockers.py
+++ lockers.py
@@ -1,70 +1,28 @@
-import threading
-import time
 import gi
 
 gi.require_version("Gtk", "3.0")
-from gi.repository import GLib, Gtk, GObject
-gi.require_version('WebKit2', '4.0')
-from gi.repository import WebKit2
-
-import sys
-import os
-from django.core.wsgi import get_wsgi_application
-from django.core.management.commands import runserver
+from gi.repository import Gtk
 
 
-PORT = 8090
-IP = '127.0.0.1'
-ROOT_URL = f'http://{IP}:{PORT}'
- 
-def app_main():
-    win = Gtk.Window(default_height=800, default_width=600)
-    win.connect("destroy", Gtk.main_quit)
+class MyWindow(Gtk.Window):
+    def __init__(self):
+        super().__init__(title="Hello World")
 
-    webView = WebKit2.WebView()
-    win.add(webView)
+        self.button = Gtk.Button(label="Click Here")
+        self.button.connect("clicked", self.on_button_clicked)
+        self.add(self.button)
 
-    webView.load_uri(ROOT_URL) # Here it works on main thread
-
-    def start_django():
-        django = runserver
-        GLib.idle_add(django.run(port=PORT,addr=bytearray(IP,"ascii"),wsgi_handler=get_wsgi_application()) )
-
-
-    win.show_all()
-    thread = threading.Thread(name = "Django", target=start_django)
-    thread.daemon = True
-    thread.start()
-
-
-
+    def on_button_clicked(self, widget):
+        print("Hello World")
 
 def main(argv=None):
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cl.settings')
-    try:
-        from django.core.management import execute_from_command_line
-
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc    
-    app_main()
-    Gtk.main()
+ win = MyWindow()
+ win.connect("destroy", Gtk.main_quit)
+ win.show_all()
+ Gtk.main()
 
 if __name__ == "__main__":
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cl.settings')
-    try:
-        from django.core.management import execute_from_command_line
-
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc    
-    app_main()
-    Gtk.main()
+ win = MyWindow()
+ win.connect("destroy", Gtk.main_quit)
+ win.show_all()
+ Gtk.main()
